8506a992deaacc7891aa18fe3c24aafb10b5a2f6
J0K3R25 <pivanov728@gmail.com>
2023-09-21 21:35:24 +0800

    add PlayerMovement script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody2D rb;
+    private Vector2 moveDirection;
+
+    private void Update()
+    {
+        ProcessInput(); 
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void ProcessInput()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        moveDirection = new Vector2(moveX, moveY);
+    }
+    private void Move()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+}
