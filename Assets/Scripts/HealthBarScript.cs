b086376635e83c1eab421bd0ec290d5cfcd20dde
awdse22 <145016508+awdse22@users.noreply.github.com>
2023-10-12 21:30:50 +0800

    Script
    Updated Health Bar Script

--- Assets/Scripts/HealthBarScript.cs
+++ Assets/Scripts/HealthBarScript.cs
@@ -2,20 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class HealthBar : MonoBehaviour
 {
-    public Slider healthBar;
-    public Health playerHealth;
-    private void Start()
+    private float maxHealth = 100f;
+    private Slider healthBar;
+    void Start()
     {
-        playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<Health>();
         healthBar = GetComponent<Slider>();
-        healthBar.maxValue = playerHealth.maxHealth;
-        healthBar.value = playerHealth.maxHealth;
+        healthBar.maxValue = maxHealth;
     }
-    public void SetHealth(int hp)
+
+    // Update is called once per frame
+    void Update()
     {
-        healthBar.value = hp;
+        healthBar.value = PlayerManager.health;
     }
 }
-Code language: C# (cs)
