58074b06c1b5d8dedc68b005a86cdfd97d1a971f
J0K3R25 <pivanov728@gmail.com>
2023-10-12 20:27:30 +0800

    Add PlayerManager
    keeps track of health and drunklevel, keeps decreasing drunkLevel, player looses health when it reaches zero. Scene gets reloaded when health reaches 0

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerManager : MonoBehaviour
+{
+    private GameObject player;
+    [SerializeField] float initialDrunkLevel;
+    private float drunkLevel;
+    private float health;
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        drunkLevel = initialDrunkLevel;
+        health = 100f;
+    }
+
+    private void FixedUpdate()
+    {
+        if (drunkLevel > 0)
+        {
+            drunkLevel -= 0.1f;
+        } else if (health > 0)
+        {
+            health -= 0.2f;
+        } else
+        {
+            Dead();
+        }
+        print("\n health: " + health + " drunkLevel: " + drunkLevel);
+    }
+    private void Dead()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
